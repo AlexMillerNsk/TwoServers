a5ac0047255bb9dcbe9c3abeaa802df8c749da33
AlexMillerNsk <love.me.lnikov@gmail.com>
2022-08-04 09:44:04 +0700

    Добавьте файлы проекта.

---
+++ Server2/Program.fs
@@ -0,0 +1,24 @@
+﻿module Server
+
+open System.IO
+open Suave
+open Suave.Utils
+open Filters
+open Operators
+
+let greetings q =
+  defaultArg (Option.ofChoice (q ^^ "name")) "World" |> sprintf "Hello %s"
+let app: WebPart =
+     choose [
+          GET >=> path "/" >=> Successful.OK "Hello GET"   
+          GET >=> path "/test" >=> Successful.OK "Biden666"
+          GET  >=> request (fun r -> Successful.OK (greetings r.query))
+          RequestErrors.NOT_FOUND "Page not found." ]
+
+let config = {
+    defaultConfig with
+        homeFolder = Some(Path.GetFullPath "./public")
+}
+
+startWebServer config app
+
---
+++ TwoServers/Program.fs
@@ -0,0 +1,20 @@
+﻿module Server
+
+open System.IO
+open Suave
+open Filters
+open Operators
+
+let app: WebPart =
+     choose [
+          GET >=> path "/" >=> Files.file "./public/index.html"    
+          GET >=> path "/test" >=> Successful.OK "Biden666"
+          RequestErrors.NOT_FOUND "Page not found." ]
+
+let config = {
+    defaultConfig with
+        bindings = [HttpBinding.createSimple Protocol.HTTP "192.168.68.100" 8080]
+        homeFolder = Some(Path.GetFullPath "./public")
+}
+
+startWebServer config app
